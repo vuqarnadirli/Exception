1cb6e45da0d1567a41b0422ce151b5525387762c
Vuqar <nadirlivuqar@gmail.com>
2022-12-11 00:57:31 +0400

    Manually writing the printStackTrace method

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        foo();
+    }
+
+    public static void foo() {
+        try {
+            int[] arr = {};
+            System.out.println(arr[0]);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+//            System.out.println(ex.getClass().getName());
+//
+//            System.out.println("Xeta bas verdi!");
+
+            StackTraceElement[] st = ex.getStackTrace();
+            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
+            for (int i = 0; i < st.length; i++) {
+                StackTraceElement stE = st[i];
+                System.err.println("\tat main.Main." + stE.getMethodName() + "(" + stE.getFileName() + ":" + stE.getLineNumber() + ")");
+
+            }
+        }
+    }
+}
