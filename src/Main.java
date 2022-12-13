b06a09447a81c4b7ed7baece32017035fa4d419f
Vuqar <nadirlivuqar@gmail.com>
2022-12-13 23:46:55 +0400

    checked exception and catching by jwm

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            foo();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws SQLException {
+        foo();
     }
 
 
