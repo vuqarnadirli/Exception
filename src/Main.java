7a971a47d16f11ec8fac79f3f8211fd34109cdba
Vuqar <nadirlivuqar@gmail.com>
2022-12-13 23:43:38 +0400

    checked exception and catching by user with try-catch

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,17 @@
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
-        String str = null;
-
-        if (str == null) {
-            try {
-                throw new MyException("Exception");
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+        try {
+            foo();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-
-        //foo();
     }
 
-    public static void foo() {
-        try {
-            int[] arr = {};
-            System.out.println(arr[0]);
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-
-//            System.out.println(ex.getClass().getName());
-//
-//            System.out.println("Xeta bas verdi!");
-
-            StackTraceElement[] st = ex.getStackTrace();
-            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
-            for (int i = 0; i < st.length; i++) {
-                StackTraceElement stE = st[i];
-                System.err.println("\tat main.Main." + stE.getMethodName() + "(" + stE.getFileName() + ":" + stE.getLineNumber() + ")");
-
-            }
-        }
+    public static  void foo() throws SQLException {
+        //throw new ArithmeticException(); unchecked exception
+        throw  new SQLException(); //checked exception
     }
 }
